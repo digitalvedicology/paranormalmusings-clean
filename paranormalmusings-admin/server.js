2e798f19ebbd3e86d4b8101887609d9235b5e6f8
Sanjai <digitalvedicology@gmail.com>
2026-09-03 15:20:44 +0530

    Fix: Use correct server.js format for Hostinger and set admin port to 3000

--- paranormalmusings-admin/server.js
+++ paranormalmusings-admin/server.js
@@ -1,17 +1,12 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = Number(process.env.PORT || 3000);
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3001, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3001')
-  })
-})
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`Admin app running on port ${port}`);
+  });
+});
--- paranormalmusings-frontend/server.js
+++ paranormalmusings-frontend/server.js
@@ -1,17 +1,12 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = Number(process.env.PORT || 3000);
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`Frontend app running on port ${port}`);
+  });
+});
